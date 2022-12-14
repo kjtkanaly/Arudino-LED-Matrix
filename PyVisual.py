f298094dda59475ad525d24e6e6fb58e58887714
kjtkanaly <kjtkanaly@gmail.com>
2022-12-14 09:19:33 -0500

    Progress on downsampling images

--- PyVisual.py
+++ PyVisual.py
@@ -1,17 +1,13 @@
 import cv2
 import numpy as np
-from matplotlib import image
+from matplotlib import pyplot as plt
 
 def main():
     img = cv2.imread("TestImage.png")
 
-    cv2.imshow("Test Image",img)
-    cv2.waitKey( )
-    cv2.destroyAllWindows
-
     imgData = np.asarray(img)
 
-    downSampleRate = 4
+    downSampleRate = 10
     downImgData = np.zeros((round(imgData.shape[0] / downSampleRate),
                            round(imgData.shape[1] / downSampleRate),
                            3))
@@ -22,21 +18,26 @@ def main():
     for row in range(0,img.shape[0],downSampleRate):
         for col in range(0,img.shape[1],downSampleRate):
             avgRedValue = np.mean(imgData[row:row + downSampleRate,
-                                          col:col + downSampleRate, 1])
-            avgBlueValue = np.mean(imgData[row:row + downSampleRate,
-                                          col:col + downSampleRate, 1])
+                                          col:col + downSampleRate, 0])
             avgGreenValue = np.mean(imgData[row:row + downSampleRate,
                                           col:col + downSampleRate, 1])
+            avgBlueValue = np.mean(imgData[row:row + downSampleRate,
+                                          col:col + downSampleRate, 2])
             
-            imgRow = row / downSampleRate
-            imgCol = col / downSampleRate
+            imgRow = round(row / downSampleRate)
+            imgCol = round(col / downSampleRate)
             downImgData[imgRow, imgCol, :] = [avgRedValue, 
-                                              avgBlueValue, 
-                                              avgGreenValue]
-            
+                                              avgGreenValue, 
+                                              avgBlueValue]
 
-
+    # Plotting
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.imshow(imgData.astype(np.uint8))
+    ax1.set_title("Original")
+    ax2.imshow(downImgData.astype(np.uint8))
+    ax2.set_title("Downsample")
+    plt.show()
     
-
+    
 if __name__ == "__main__":
     main()
